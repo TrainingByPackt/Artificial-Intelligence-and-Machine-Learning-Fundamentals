56db4c55885b56474df15a8616b618dfc61bafb0
steffimonteiro <43338640+steffimonteiro@users.noreply.github.com>
2018-11-29 14:09:53 +0530

    Update poly.py

--- Lesson03/Exercise 09 1st, 2nd, and 3rd Degree Polynomial Regression/poly.py
+++ Lesson03/Exercise 09 1st, 2nd, and 3rd Degree Polynomial Regression/poly.py
@@ -26,3 +26,20 @@ plot.plot(
  x1, f3(x1)
  )
 plot.show()
+
+deg1 = np.polyfi(x2, y2, 1)
+# array([ 9.12087912, -34.92307692])
+f1 = np.poly1d(deg1)
+deg2 = np.polyfi(x2, y2, 2)
+# array([ 1.75024975, -15.38261738, 26.33566434])
+f2 = np.poly1d(deg2)
+deg3 = np.polyfi(x2, y2, 3)
+# array([0.2465035, -3.42632368, 14.69080919,
+# -15.07692308])
+f3 = np.poly1d(deg3)
+plot.plot(
+ x2, y1, 'o',# blue dots
+ x2, f1(x2), # orange
+ x2, f2(x2), # green
+ x2, f3(x2) # red
+)
