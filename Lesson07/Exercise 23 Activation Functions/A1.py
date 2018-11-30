d4fe9e75b57031a5278f0adaef1e39491eae7346
steffimonteiro <steffim@packt.com>
2018-11-30 12:26:40 +0530

    update

--- Lesson07/Exercise 23 Activation Functions/A1.py
+++ Lesson07/Exercise 23 Activation Functions/A1.py
@@ -10,33 +10,34 @@ def relu(x):
 return 0 if x < 0 else x
 
 get_y( sigmoid, -2 )
-The output is 0.5
+#The output is 0.5
 get_y(sigmoid, -1)
-The output is 0.8175744761936437
+#The output is 0.8175744761936437
 get_y(sigmoid, 0)
-The output is 0.9525741268224331
+#The output is 0.9525741268224331
 get_y(sigmoid, 1)
-The output is 0.9890130573694068
+#The output is 0.9890130573694068
 get_y(sigmoid, 2)
-The output is 0.9975273768433653
+#The output is 0.9975273768433653
 
 get_y(tanh, -2)
-The output is 0.0
+#The output is 0.0
 get_y(tanh, -1)
-The output is 0.9051482536448663
+#The output is 0.9051482536448663
 get_y(tanh, 0)
-The output is 0.9950547536867307
+#The output is 0.9950547536867307
 get_y(tanh, 1)
-The output is 0.9997532108480274
+#The output is 0.9997532108480274
 get_y(tanh, 2)
-The output is 0.9999877116507956
+#The output is 0.9999877116507956
 
+get_y(relu,-2)
 The output is 0.0
 get_y(relu,-1)
-The output is 1.5
+#The output is 1.5
 get_y(relu,0)
-The output is 3.0
+#The output is 3.0
 get_y(relu,1)
-The output is 4.5
+#The output is 4.5
 get_y(relu,2)
-The output is 6.0
+#The output is 6.0
